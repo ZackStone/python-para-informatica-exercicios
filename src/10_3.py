5c5e32a376baf4f894ba8c12971920bc04569ddf
Yitzhak Andrade <andrade@hotmail.com.br>
2017-05-11 22:06:33 -0300

    Terminado

--- src/10_3.py
+++ src/10_3.py
@@ -5,4 +5,60 @@
 # de diferentes linguagens e veja como a frequência das letras variam entre as linguagens.
 # Compare seu resultado com as tabelas em wikipedia.org/wiki/Letter_frequencies.
 
-# todo
+import os
+
+def conta(arquivo):
+
+	curpath = os.path.dirname(os.path.abspath(__file__))
+	filepath = curpath + '/../data/'
+
+	fhand = open(filepath + arquivo)
+
+	count = dict()
+	for line in fhand:
+		letras = list(line)
+		for letra in letras:
+			letra = letra.lower()
+			if letra >= 'a' and letra <= 'z':
+				if letra in count: count[letra] += 1
+				else: count[letra] = 1
+
+
+	lista = []
+	for c in count:
+		el = (count[c], c)
+		lista.append(el)
+
+	lista.sort(reverse=True)
+
+
+	soma = 0
+	for l in lista:
+		soma += l[0]
+
+	i = 1
+	for l in lista:
+		if i > 5: break
+		p = round(l[0] / soma * 100, 2)
+		print('  ' + l[1] + ' ' + str(p) + ' %')
+		i += 1
+
+print('Inglês')
+arquivo = 'bor-stat.txt'
+conta(arquivo);
+
+print('Alemão')
+arquivo = 'german.txt'
+conta(arquivo);
+
+print('Espanhol')
+arquivo = 'spanish.txt'
+conta(arquivo);
+
+print('Francês')
+arquivo = 'fr.txt'
+conta(arquivo);
+
+print('Português')
+arquivo = 'pt.txt'
+conta(arquivo);
